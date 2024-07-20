45737d157341365c686975a2b80ccaa4556d57c1
jadeharris <jadieclair@yahoo.com>
2024-07-20 15:52:19 +0200

    comment out API key

--- apiKey.js
+++ apiKey.js
@@ -1,25 +1,25 @@
-// Set the root path for the API
-let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
-// Add event listener for the submit API key button
-document.getElementById("submitApiKey").addEventListener("click", setApiKey);
+// // Set the root path for the API
+// let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
+// // Add event listener for the submit API key button
+// document.getElementById("submitApiKey").addEventListener("click", setApiKey);
 
-// Function to handle the API key submission
-function setApiKey(e) {
-  e.preventDefault();
+// // Function to handle the API key submission
+// function setApiKey(e) {
+//   e.preventDefault();
 
-  // Get the entered API key
-  apiKey = document.getElementById("apiKey").value;
-  // Send a request to validate the API key
-  fetch(rootPath + "controller/api-key/?apiKey=" + apiKey)
-    .then(function (response) {
-      return response.text();
-    })
-    .then(function (data) {
-      if (data == "1") {
-        localStorage.setItem("apiKey", apiKey);
-        window.open("index.html", "_self");
-      } else {
-        alert(data + "Invalid Api key entered!");
-      }
-    });
-}
+//   // Get the entered API key
+//   apiKey = document.getElementById("apiKey").value;
+//   // Send a request to validate the API key
+//   fetch(rootPath + "controller/api-key/?apiKey=" + apiKey)
+//     .then(function (response) {
+//       return response.text();
+//     })
+//     .then(function (data) {
+//       if (data == "1") {
+//         localStorage.setItem("apiKey", apiKey);
+//         window.open("index.html", "_self");
+//       } else {
+//         alert(data + "Invalid Api key entered!");
+//       }
+//     });
+// }
